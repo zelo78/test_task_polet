44f17284663850f6c79d7fe9a54c94441a065c33
Oleg <Zelo78@yandex.ru>
2022-07-02 03:19:19 +0300

    Add viewset

--- project/main/serializers.py
+++ project/main/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from main.models import Vehicle
@@ -8,9 +7,3 @@ class VehicleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Vehicle
         fields = "__all__"
-
-
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ["url", "username", "email", "groups"]
--- project/main/views.py
+++ project/main/views.py
@@ -1,24 +1,15 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from main.serializers import UserSerializer, VehicleSerializer
+from main.serializers import VehicleSerializer
 from main.models import Vehicle
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing the Vehicles.
+    """
+
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#
-#     queryset = User.objects.all().order_by("-date_joined")
-#     serializer_class = UserSerializer
-#     permission_classes = [permissions.IsAuthenticated]
