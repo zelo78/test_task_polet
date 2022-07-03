9fa44e563273fb69548f2946609fd2d3e5f70e84
Oleg <Zelo78@yandex.ru>
2022-07-03 13:34:17 +0300

    List display on admin site

--- project/main/admin.py
+++ project/main/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from main.models import Vehicle
 
-admin.site.register(Vehicle)
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ["brand", "model", "year_of_manufacture", "registration_number"]
+
+
+admin.site.register(Vehicle, VehicleAdmin)
